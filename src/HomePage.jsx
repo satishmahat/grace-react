3267d6e3cbe0d3cc69155f30ddce3285b2b1af84
satishmahat <mahatsatish@gmail.com>
2024-07-13 22:26:01 +0545

    added about component

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -3,12 +3,14 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Contact from "./components/Contact";
 import Services from "./components/Services";
+import About from "./components/About";
 
 
 function HomePage(){
     return(
         <>
             <Navbar/><br />
+            <About/>
             <Services/>
             <Faqs/>
             <Contact/><br /><br />
---
+++ src/components/About.jsx
@@ -0,0 +1,20 @@
+import './about.css'
+import logo from './images/logo.png';
+
+function About(){
+    return(
+        <>
+        <div className="about-us">
+            <div className="about">
+                <img src={logo} className="pic" />
+                <div className="text">
+                    <h2>About Us</h2>
+                    <h4>Grace Dental Care</h4>
+                    <p>Welcome to a place where your smile’s journey begins! We fuse cutting-edge dental technology with a gentle touch, creating a perfect harmony of comfort and innovation. Our mission is to transform every visit into a positive experience, ensuring your oral health and radiant smile are always at their best. Discover the difference, where exceptional care meets extraordinary results. Join us and experience the future of dental care today!</p>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+export default About
