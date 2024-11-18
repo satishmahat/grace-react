cdf1e918c40c19c5d5f2285b0e5315e07a6493df
satishmahat <mahatsatish@gmail.com>
2024-11-18 18:12:50 +0545

    loading animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HomePage from './HomePage'
 
+import Loading from './components/Loading.jsx';
+import './App.css';
 
-function App() {
-  
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+      const handleLoad = () => {
+          setIsLoading(false);
+      };
+
+      // Listen for the page load event
+      window.addEventListener('load', handleLoad);
+
+      // Clean up the event listener
+      return () => {
+          window.removeEventListener('load', handleLoad);
+      };
+  }, []);
+
   return (
-    <>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<HomePage/>}></Route>
-        <Route path='/home' element={<HomePage/>}></Route>
-      </Routes>
-      </BrowserRouter>
-    </>
-  )
-}
+      <>
+          {isLoading && <Loading />}
+          <div className={`app-content ${isLoading ? 'hidden' : ''}`}>
+          <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<HomePage/>}></Route>
+            <Route path='/home' element={<HomePage/>}></Route>
+          </Routes>
+          </BrowserRouter>
+          </div>
+      </>
+  );
+};
 
-export default App
+export default App;
